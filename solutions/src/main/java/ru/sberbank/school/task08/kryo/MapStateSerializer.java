65d0c0ca4c22e369a7004a40b72905597949a939
Egor Bzhosko <lansxkeku@mail.ru>
2019-06-15 15:35:41 +0300

    id:8 push

--- solutions/src/main/java/ru/sberbank/school/task08/kryo/MapStateSerializer.java
+++ solutions/src/main/java/ru/sberbank/school/task08/kryo/MapStateSerializer.java
@@ -17,34 +17,17 @@ public class MapStateSerializer extends Serializer<MapState<GameObject>> {
     public void write(Kryo kryo, Output output, MapState<GameObject> object) {
         output.writeString(object.getName());
 
-        int volume = object.getGameObjects().size();
-        output.writeInt(volume);
-
-        for (GameObject gameObject : object.getGameObjects()) {
-            kryo.writeClassAndObject(output, gameObject.getType());
-            kryo.writeClassAndObject(output, gameObject.getStatus());
-            kryo.writeClassAndObject(output, gameObject.getHitPoints());
-        }
+        kryo.writeClassAndObject(output, object.getGameObjects());
 
     }
 
     @Override
     public MapState read(Kryo kryo, Input input, Class<? extends MapState<GameObject>> type) {
 
-
         String name = input.readString();
-        int volume = input.readInt();
 
-        List<GameObject> list = new ArrayList<>();
-
-        for (int i = 0; i < volume; i++) {
-            InstantiatableEntity.Type type1 = (InstantiatableEntity.Type) kryo.readClassAndObject(input);
-            InstantiatableEntity.Status status = (InstantiatableEntity.Status) kryo.readClassAndObject(input);
-            long hitPoints = (long) kryo.readClassAndObject(input);
-
-            GameObject gameObject = new GameObject(type1, status, hitPoints);
-            list.add(gameObject);
-        }
+        @SuppressWarnings("unchecked")
+        List<GameObject> list = (List<GameObject>) kryo.readClassAndObject(input);
 
         return new MapState(name, list);
     }
